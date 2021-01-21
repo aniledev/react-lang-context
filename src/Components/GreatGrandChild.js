57d769180e69d99ce9d49629f57cb7e3b0cd5f19
aniledev <elinamcgill@gmail.com>
2021-01-21 15:18:59 -0500

    REFACTOR: GreatGrandChild component to use context instead of hardcorded language

--- src/Components/GreatGrandChild.js
+++ src/Components/GreatGrandChild.js
@@ -3,13 +3,17 @@
 
 import React, { Component } from "react";
 import languageSpecificCopy from "../languageSpecificCopy";
+import LanguageContext from "./LanguageContext";
 
 class GreatGrandChild extends Component {
+  // attach context to class component; assign context to static property called contextType, gives us a new instance property this.context
+  static contextType = LanguageContext;
+
   render() {
     // We're using an "or an empty object" logic when creating the copy constant with the syntax || {}
     // similar to default props but the data file is not a prop that we can use
     // if we attempt to use a key that doesn't exist, it would equate to undefined and this would throw errors all over our app
-    const copy = languageSpecificCopy["en-US"] || {};
+    const copy = languageSpecificCopy[this.context.lang] || {};
     return (
       <section className="GreatGrandChild">
         <h2>{copy.title}</h2>
