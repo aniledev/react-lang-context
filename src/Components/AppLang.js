c348d594bafba2ba986c4a75674c73b2cec450e5
aniledev <elinamcgill@gmail.com>
2021-01-21 15:04:52 -0500

    FEAT: import child component into AppLang component

--- src/Components/AppLang.js
+++ src/Components/AppLang.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import "../Components/App.css";
+import Child from "./Child";
 
 export default class AppLang extends Component {
   render() {
-    return <div className="AppLang">App context</div>;
+    return (
+      <div className="AppLang">
+        <Child />
+      </div>
+    );
   }
 }
