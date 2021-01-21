05444a66649b8584c61e7b52163e7458d61cc4bb
aniledev <elinamcgill@gmail.com>
2021-01-21 15:01:40 -0500

    FEAT: flesh out GreatGrandChild component

---
+++ src/Components/GreatGrandChild.js
@@ -0,0 +1,21 @@
+//  In order to use context this will need to be a class component.
+// this component will render the text in different languages based on language controls
+
+import React, { Component } from "react";
+import languageSpecificCopy from "../languageSpecificCopy";
+
+class GreatGrandChild extends Component {
+  render() {
+    // We're using an "or an empty object" logic when creating the copy constant with the syntax || {}
+    // similar to default props but the data file is not a prop that we can use
+    const copy = languageSpecificCopy["en-US"] || {};
+    return (
+      <section className="GreatGrandChild">
+        <h2>{copy.title}</h2>
+        <p>{copy.body}</p>
+      </section>
+    );
+  }
+}
+
+export default GreatGrandChild;
