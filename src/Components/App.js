d6e42a2bd50f7048f366b7a6ff4c38000cb4333a
aniledev <elinamcgill@gmail.com>
2021-01-21 14:55:25 -0500

    FEAT: delete create react app boilerplate and set up AppLang.js component

---
+++ src/Components/App.js
@@ -0,0 +1,9 @@
+import "../Components/App.css";
+
+import React, { Component } from "react";
+
+export default class AppLang extends Component {
+  render() {
+    return <div className="AppLang">App context</div>;
+  }
+}
