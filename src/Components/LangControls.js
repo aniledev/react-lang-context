277de831e9005243527e05a93351407b80cd9eb8
aniledev <elinamcgill@gmail.com>
2021-01-21 15:33:21 -0500

    FEAT: use value from render props to disable the active button using truthiness of value.lang conditional

--- src/Components/LangControls.js
+++ src/Components/LangControls.js
@@ -1,4 +1,5 @@
 // this component contains buttons to switch the current language
+// Consumer allows us to use context in function components
 
 import React from "react";
 import LanguageContext from "./LanguageContext";
@@ -9,22 +10,22 @@ export default function LangControls(props) {
     // move the JSX we want to return into the return from the render
     <LanguageContext.Consumer>
       {(value) => {
-        console.log(value);
         return (
           <>
-            <button>
+            <button disabled={value.lang === "en-GB"}>
+              {" "}
               British{" "}
               <span role="img" aria-label="en-GB">
                 🇬🇧
               </span>
             </button>{" "}
-            <button>
+            <button disabled={value.lang === "en-US"}>
               American{" "}
               <span role="img" aria-label="en-US">
                 🇺🇸
               </span>
             </button>{" "}
-            <button>
+            <button disabled={value.lang === "ko"}>
               Korean{" "}
               <span role="img" aria-label="ko">
                 🇰🇷
