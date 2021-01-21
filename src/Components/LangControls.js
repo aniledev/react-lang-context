1c82a5f1abe4fb9e70a073383d3d008f4c0de6ff
aniledev <elinamcgill@gmail.com>
2021-01-21 15:24:21 -0500

    FEAT: add LanguageContext.Consume component into LangControls component

--- src/Components/LangControls.js
+++ src/Components/LangControls.js
@@ -1,28 +1,35 @@
 // this component contains buttons to switch the current language
 
 import React from "react";
+import LanguageContext from "./LanguageContext";
 
 export default function LangControls(props) {
   return (
-    <>
-      <button>
-        British{" "}
-        <span role="img" aria-label="en-GB">
-          🇬🇧
-        </span>
-      </button>{" "}
-      <button>
-        American{" "}
-        <span role="img" aria-label="en-US">
-          🇺🇸
-        </span>
-      </button>{" "}
-      <button>
-        Korean{" "}
-        <span role="img" aria-label="ko">
-          🇰🇷
-        </span>
-      </button>
-    </>
+    <LanguageContext.Consumer>
+      {function renderProp() {
+        return (
+          <>
+            <button>
+              British{" "}
+              <span role="img" aria-label="en-GB">
+                🇬🇧
+              </span>
+            </button>{" "}
+            <button>
+              American{" "}
+              <span role="img" aria-label="en-US">
+                🇺🇸
+              </span>
+            </button>{" "}
+            <button>
+              Korean{" "}
+              <span role="img" aria-label="ko">
+                🇰🇷
+              </span>
+            </button>
+          </>
+        );
+      }}
+    </LanguageContext.Consumer>
   );
 }
