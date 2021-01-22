98901d58ac1869d332bdecb9b9f56d91b8095f11
aniledev <elinamcgill@gmail.com>
2021-01-21 21:34:04 -0500

    CHORE: spell check document

--- src/Components/LanguageContext.js
+++ src/Components/LanguageContext.js
@@ -1,5 +1,5 @@
 // use this component to creat context for the language
-// contet should be in its own file so it can be imported into any other file
+// context should be in its own file so it can be imported into any other file
 
 import React from "react";
 
