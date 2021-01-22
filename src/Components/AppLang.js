a36f46084c85f44a9cd4ff48302c9e44e4c08825
aniledev <elinamcgill@gmail.com>
2021-01-21 20:23:15 -0500

    FEAT: move the value into the component state so we can update it

--- src/Components/AppLang.js
+++ src/Components/AppLang.js
@@ -2,14 +2,26 @@ import React, { Component } from "react";
 import "../Styles/AppLang.css";
 import Child from "./Child";
 import LangControls from "./LangControls";
+import LanguageContext from "./LanguageContext";
 
+// wrap the subtree that needs the updated context values in the LanguageContext.Provider
 export default class AppLang extends Component {
+  state = {
+    lang: window.navigator.language,
+  };
+
   render() {
+    const contextValue = {
+      lang: this.state.lang,
+    };
+
     return (
-      <div className="AppLang">
-        <LangControls />
-        <Child />
-      </div>
+      <LanguageContext.Provider value={contextValue}>
+        <div className="AppLang">
+          <LangControls />
+          <Child />
+        </div>
+      </LanguageContext.Provider>
     );
   }
 }
